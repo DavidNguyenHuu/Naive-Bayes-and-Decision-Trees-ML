e1ed81476fe8cf4d8ed1e1b1bb74a3353c4e20ff
DavidNguyenHuu <37845640+DavidNguyenHuu@users.noreply.github.com>
2021-02-16 18:42:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,40 +1,48 @@
 from __future__ import division
-from __future__ import division
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import confusion_matrix
-from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, plot_confusion_matrix
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.metrics import precision_recall_fscore_support
 from sklearn.metrics import accuracy_score
 from collections import Counter
+import matplotlib.pyplot as plt
+from codecs import open
 
+
+# We will start by by defining a new function we will call read documents
+
+def read_documents(doc_file):
+    docs = []  # this array will contain the reviews
+    labels = []  # this array will contain the labels
+    with(open(doc_file, encoding='utf-8'))as f:
+        for line in f:
+            words = line.strip().split()  # splitting each line
+            docs.append(words[3:])  # adding only the review to the array docs
+            labels.append(words[1])  # adding only the labels to the array labels
+    return docs, labels
 
 # we will split the data into 2 sets
 all_docs, all_labels = read_documents('all_sentiment_shuffled.txt')
-count_vec= TfidfVectorizer(tokenizer=lambda x: x, preprocessor=lambda x: x)
+count_vec= CountVectorizer(tokenizer=lambda x: x, preprocessor=lambda x: x)
 vec=count_vec.fit_transform(all_docs)
 X_train, X_test, y_train, y_test = train_test_split(vec,all_labels,test_size=0.2, random_state=0)
-count_vec = CountVectorizer(tokenizer=lambda x: x, preprocessor=lambda x: x)
-vec = count_vec.fit_transform(all_docs)
-X_train, X_test, y_train, y_test = train_test_split(vec, all_labels, test_size=0.2, random_state=0)
 
 
 # Task 1
 # This method computes the frequency of the instances in each class
 def get_frequency(data):
-
+    frequency = Counter()
+    for label in data:
+        frequency[label] += 1
+    return frequency
 
 
 # Plot the distribution of the number of the instances in each class.
-count= get_frequency(all_labels)
-print(count)
-plt.title(label='Distribution of the number of the instances',fontsize=12,
 count = get_frequency(all_labels)
 plt.title(label='Distribution of the complete Dataset', fontsize=10,
           color="black")
-plt.bar(count.keys(), count.values(),color='green')
 plt.bar(count.keys(), count.values(), color='green')
 plt.xlabel('Sentiment')
 plt.ylabel('Frequency')
