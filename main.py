11610a3a9f8a4060dd788d5b5413004b9a1db5be
henrydd98 <henrydd.98@gmail.com>
2021-02-20 22:33:01 -0500

    Add display misclassified reviews

--- main.py
+++ main.py
@@ -8,11 +8,10 @@ from sklearn.metrics import precision_recall_fscore_support
 from sklearn.metrics import accuracy_score
 from collections import Counter
 import matplotlib.pyplot as plt
+import numpy as np
 from codecs import open
 
-
 # We will start by by defining a new function we will call read documents
-
 def read_documents(doc_file):
     docs = []  # this array will contain the reviews
     labels = []  # this array will contain the labels
@@ -23,14 +22,12 @@ def read_documents(doc_file):
             labels.append(words[1])  # adding only the labels to the array labels
     return docs, labels
 
-
-# we will split the data into 2 sets
+# We will split the data into 2 sets
 all_docs, all_labels = read_documents('all_sentiment_shuffled.txt')
+indices = np.arange(len(all_docs))
 count_vec = CountVectorizer(tokenizer=lambda x: x, preprocessor=lambda x: x)
 vec = count_vec.fit_transform(all_docs)
-X_train, X_test, y_train, y_test = train_test_split(vec, all_labels, test_size=0.2, random_state=0)
-
-
+X_train, X_test, y_train, y_test, i_train, i_test = train_test_split(vec, all_labels, indices, test_size=0.2, random_state=0) #i_train and i_test for indices
 
 # Task 1
 # This method computes the frequency of the instances in each class
@@ -39,7 +36,6 @@ def get_frequency(data):
     for label in data:
         frequency[label] += 1
     return frequency
-
 
 # Plot the distribution of the number of the instances in each class.
 count = get_frequency(y_train)
@@ -57,7 +53,10 @@ clf.fit(X_train, y_train)  # assigning each review to a label
 clf_pred = clf.predict(X_test)  # create an array containing the the test reviews with the predicated labels
 text_file = open("output_NB.txt", "w")
 for row, classes in enumerate(clf_pred):
-  text_file.write(str(row+1)+","+str(classes)+"\n")
+    if classes != y_test[row]:
+        print("Row number", i_test[row], 'has been classified as', classes, 'but should be', y_test[row])
+        print("It contains:", all_docs[i_test[row]], "\n")
+    text_file.write(str(row+1)+","+str(classes)+"\n")
 ac_NB = accuracy_score(y_test, clf_pred)
 text_file.write("The accuracy of Naive Bayes is: %s\n" % ac_NB)
 PRFS_NB = str(precision_recall_fscore_support(y_test, clf_pred, average='weighted'))
@@ -65,13 +64,8 @@ text_file.write("The precision, recall , fscore for Naive Bayes are :" + PRFS_NB
 text_file.write("Confusion Matrix: \n")
 text_file.write(str(confusion_matrix(y_test, clf_pred)) + "\n")
 text_file.close()
-
-
-
 plot_confusion_matrix(clf, X_test, y_test)
 plt.show()
-
-
 
 # Base Decision Tree
 tree1 = DecisionTreeClassifier()  # creating a tree
@@ -89,7 +83,6 @@ text_file.write(str(confusion_matrix(y_test, tree1_pred)) + "\n")
 text_file.close()
 plot_confusion_matrix(tree1, X_test, y_test)
 plt.show()
-
 
 # Best decision tree
 tree2 = DecisionTreeClassifier(
